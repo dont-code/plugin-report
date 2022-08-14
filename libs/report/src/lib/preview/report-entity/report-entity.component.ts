3f28648b408e1c08e02c92489f2dc93b7ecddc18
Gérard Collin <gerard.collin@appliweb.net>
2022-08-14 10:18:23 +0200

    Report page displaying

--- libs/report/src/lib/preview/report-entity/report-entity.component.ts
+++ libs/report/src/lib/preview/report-entity/report-entity.component.ts
@@ -1,11 +1,11 @@
-import { Component, Injector } from '@angular/core';
+import {ChangeDetectorRef, Component, Injector, TemplateRef, ViewChild} from '@angular/core';
 import {
-  ComponentLoaderService,
+  ComponentLoaderService, FormElement,
   PluginBaseComponent,
   PossibleTemplateList,
   TemplateList,
 } from '@dontcode/plugin-common';
-import { Change } from '@dontcode/core';
+import {Change, CommandProviderInterface, DontCodeModel, DontCodeModelPointer} from '@dontcode/core';
 
 @Component({
   selector: 'dontcode-report-entity',
@@ -13,8 +13,33 @@ import { Change } from '@dontcode/core';
   styleUrls: ['./report-entity.component.scss'],
 })
 export class ReportEntityComponent extends PluginBaseComponent {
-  constructor(loader: ComponentLoaderService, injector: Injector) {
+
+  @ViewChild('defaultdisplay')
+  private defaultTemplate!: TemplateRef<any>;
+
+  title="No Title";
+
+  constructor(loader: ComponentLoaderService, injector: Injector,     private ref: ChangeDetectorRef
+  ) {
     super(loader, injector);
+  }
+
+  override initCommandFlow(
+    provider: CommandProviderInterface,
+    pointer: DontCodeModelPointer
+  ): any {
+    //this.initing=true;
+    super.initCommandFlow(provider, pointer);
+
+    if (!this.entityPointer)
+      throw new Error(
+        'Cannot listen to changes without knowing a base position'
+      );
+    this.decomposeJsonToMultipleChanges(
+      this.entityPointer,
+      provider.getJsonAt(this.entityPointer.position)
+    ); // Dont provide a special handling for initial json, but emulate a list of changes
+    this.initChangeListening(true); // Listen to all changes occuring after entityPointer
   }
 
   providesTemplates(key?: string): TemplateList {
@@ -25,6 +50,21 @@ export class ReportEntityComponent extends PluginBaseComponent {
   }
 
   handleChange(change: Change): void {
-    // Do nothing for now
+    if (change?.pointer?.positionInSchema === DontCodeModel.APP_REPORTS_DISPLAY) {
+      this.updateSubFieldsWithChange(change, DontCodeModel.APP_REPORTS_DISPLAY_NODE).then(value => {
+        if (value != null) {
+          this.ref.markForCheck();
+          this.ref.detectChanges();
+        }
+      })
+    }
   }
+
+  templateOf(field: FormElement): TemplateRef<any> {
+    let ref = field.component?.providesTemplates(field.type).forFullView;
+    if (!ref) ref = this.defaultTemplate;
+
+    return ref;
+  }
+
 }
