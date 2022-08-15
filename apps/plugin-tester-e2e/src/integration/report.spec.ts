e14eb80a632306f86c4955e7f6f80fcc2e07e70e
Gérard Collin <gerard.collin@appliweb.net>
2022-08-15 10:22:08 +0200

    Fixed e2é test

--- apps/plugin-tester-e2e/src/integration/report.spec.ts
+++ apps/plugin-tester-e2e/src/integration/report.spec.ts
@@ -12,50 +12,17 @@ import {
 describe('plugin-tester', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display Report Field', () => {
+  it('should display Basic Report', () => {
     cy.intercept('GET', '/assets/dev/templates.json').as('LoadTemplate');
     getSubMenuWithText('Dev').click(); // Move to dev page
 
     cy.wait('@LoadTemplate');
     clickAutoComplete('template');
-    selectPopupChoiceWithText('Test Report Field');
+    selectPopupChoiceWithText('Test Basic Report');
     getSendButton().click();
-    getSubMenuWithText('Report Field').click();
+    getSubMenuWithText('Basic Report').click();
 
-    getDropdownWithName('Test').click();
-    getDropdownListItemWithName('Rosa Report').click();
-
-    getContentArea().contains('Value of Test: Rosa');
+    getContentArea().contains('Basic Report');
   });
 
-  it('should display Reporter entity', () => {
-    cy.intercept('GET', '/assets/dev/templates.json').as('LoadTemplate');
-    getSubMenuWithText('Dev').click(); // Move to dev page
-
-    cy.wait('@LoadTemplate');
-    clickAutoComplete('template');
-    selectPopupChoiceWithText('Test Reported Entity');
-    getSendButton().click();
-    getSubMenuWithText('Reported').click();
-
-    getContentArea().contains('Hello from ReportedEntityComponent !');
-  });
-
-  it('should still display other entities', () => {
-    cy.intercept('GET', '/assets/dev/templates.json').as('LoadTemplate');
-    getSubMenuWithText('Dev').click(); // Move to dev page
-
-    cy.wait('@LoadTemplate');
-    clickAutoComplete('template');
-    selectPopupChoiceWithText('Test Other Entity');
-    getSendButton().click();
-    getSubMenuWithText('Not Reported').click();
-
-    getInputWithName('id').type('abc');
-    getInputWithName('count').type('124');
-    getInputWithName('check').click();
-
-    getContentArea().contains('Value of id: abc');
-    getContentArea().contains('Value of count: 124');
-  });
 });
